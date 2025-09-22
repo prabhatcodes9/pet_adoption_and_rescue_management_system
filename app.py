9c0fdacb039fe4076ee4a027f5e4898b0b9f47dc
prabhatcodes9 <prabhatkumar1065@gmail.com>
2025-09-22 17:46:55 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,94 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.secret_key = 'my-secret-key'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Prabhat%400@localhost/pet_rescue_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    mobile = db.Column(db.String(15), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(20), default="user")
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        mobile = request.form.get('mobile')
+        email = request.form.get('email')
+        address = request.form.get('address')
+        password = bcrypt.generate_password_hash(request.form.get['password']).decode('utf-8')
+
+        user = User(name=name, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Account created Successfully!')
+        return redirect(url_for('login'))
+    
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            flash('Login Successfully!')
+            if user.role == 'admin':
+                return redirect(url_for('admin_dashboard'))
+            else:
+                return redirect(url_for('dashboard'))
+        else:
+            flash('Invalid credentials!')
+
+    return render_template('login.html')
+
+@app.route('/user/dashboard')
+@login_required
+def user_dashboard():
+    if current_user.role != "user":
+        flash("Access denied!", "danger")
+        return redirect(url_for('login'))
+    return render_template('dashboard.html', user=current_user)
+
+@app.route('/admin/dashboard')
+@login_required
+def admin_dashboard():
+    if current_user.role != "admin":
+        flash("Access denied!", "danger")
+        return redirect(url_for('login'))
+    return render_template('admin_dashboard.html', user=current_user)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You have logged out.", "info")
+    return redirect(url_for('login'))
